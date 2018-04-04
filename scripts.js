1cda5dd79e8b1a230d853732c2c3e35947ea596c
Haley Jacobs <haleyljacobs@gmail.com>
2018-04-03 20:57:49 -0600

    another commit

--- scripts.js
+++ scripts.js
@@ -1,33 +1,113 @@
-var titleInput = document.querySelector('.title-input');
-var bodyInput = document.querySelector('.body-input');
-var saveBtn = document.querySelector('.save-button');
-var topSection = document.querySelector('.top-section');
-var bottomSection = document.querySelector('.bottom-section');
-var searchInput = document.querySelector('.search-input');
-saveBtn.addEventListener('click', createIdea);
+$titleInput = $('.title-input');
+$bodyInput = $('.body-input');
+$saveBtn = $('.save-button');
+$topSection = $('.top-section');
+$bottomSection = $('.bottom-section');
+$searchInput = $('.search-input');
+$deleteBtn = $('.delete-button');
+$upvoteBtn = $('.upvote-button')
+$downvoteBtn = $('.downvote-button')
+var cards = [];
+
+
+
+$saveBtn.on('click', createIdea);
+$bottomSection.on('click', '.delete-button', deleteCard);
+$bottomSection.on('click', '.upvote-button', increaseQuality);
+$bottomSection.on('click', '.downvote-button', decreaseQuality);
+
+
+function getIdeas() {
+  for (var i=0; i < localStorage.length; i++) {
+    // console.log(i, localStorage, localStorage.key(i))
+    var stored = localStorage.getItem(localStorage.key(i))
+    console.log(stored, 'stored')
+    var parsedCard = JSON.parse(stored); 
+    console.log('parsed', parsedCard);
+    prependIdea(parsedCard);
+  }
+};
+
+getIdeas();
 
 function createIdea() {
   event.preventDefault();
-  var idea=document.createElement('article');
-  idea.setAttribute('class', 'idea-body');
-  prependIdea(bottomSection, idea);
-  titleInput.value = '';
-  bodyInput.value='';
+  var card = new Card($titleInput.val(),  $bodyInput.val());
+  cards.push(card);
+  prependIdea(card);
+  storeIdea(card);
+ 
 }
   
-function prependIdea(bottomSection, article) {
- bottomSection.prepend(article);
- article.innerHTML = (`
-   <article class="idea-title-article" id=${Date.now()}>
-   <h2>${titleInput.value}</h2>
-   <div class="idea-body-article">
-   <h3>${bodyInput.value}</h3>
-   <p>quality: </p>
+function Card(title, body, id, quality) {
+    this.title = title;
+    this.body = body;
+    this.id = Date.now();
+    this.quality = 'swill';
+}
+
+function prependIdea(card) {
+ $bottomSection.prepend (`
+   <article class="idea-card" id=${card.id}>
+   <h2 contenteditable>${card.title}</h2>
+  
+   <h3 contenteditable>${card.body}</h3>
+   <p id='quality-statement'>quality:<span id='quality'> ${card.quality} </p>
+   <img src = 'icons/upvote.svg' class='upvote-button' width="20px" height='20px'>
+   <img src = 'icons/downvote.svg' class='downvote-button' width='20px' height='20px'>
+   <img src = 'icons/delete.svg' class='delete-button' width='20px' height='20px'>
    <hr id='idea-underline'>
+   
    </article>`);
 }
+
+function storeIdea(card) {
+  var stringifyCard = JSON.stringify(card);
+  localStorage.setItem(card.id, stringifyCard);
+}
+
 
 
 // function searchIdea() {
 
 // }
+
+function decreaseQuality() {
+  var decreaseQuality = $(this).closest('.idea-card');
+  var cardId = decreaseQuality.attr('id');
+  decreaseQuality.quality.innerHTML = "quality: plausible";
+  quality.innerText = "quality: swill";
+  console.log('decrease')
+}
+
+function increaseQuality() {
+  var increaseQuality = $(this).closest('.idea-card');
+  var cardId = increaseQuality.attr('id');
+  quality.innerText = "quality: plausible";
+  quality.innerText = "quality: genius";
+  console.log('quality')
+}
+
+// function increaseQuality() {
+//   var increaseQuality = $(this).closest('.idea-card');
+//   var cardId = increaseQuality.attr('id');
+//     if (card.quality.innerText = 'swill') {
+//       card.quality.innerText = 'plausible'
+//     } else if {
+//       (card.quality.innerText ='plausible') {
+//         card.quality.innerText = 'genius'
+//       }
+//       else {
+//       card.quality.innerText = 'genius'
+//     }
+// }
+
+function deleteCard() {
+  var deleteCard = $(this).closest('.idea-card');
+  var cardId = deleteCard.attr('id');
+  console.log(deleteCard);
+  console.log(cardId);
+  deleteCard.remove();
+  localStorage.removeItem(cardId);
+
+}
