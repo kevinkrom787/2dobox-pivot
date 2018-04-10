c3c88b142b2ab0a341b93293b1c3b0a22cb70704
Kevin Krom <kevinkrom787@gmail.com>
2018-04-09 20:22:05 -0600

    Add disabled button

--- scripts.js
+++ scripts.js
@@ -1,21 +1,34 @@
 
-$saveBtn = $('.save-button');
-$bottomSection = $('.bottom-section');
-$searchInput = $('.search-input');
-$deleteBtn = $('.delete-button');
-$upvoteBtn = $('.upvote-button')
-$downvoteBtn = $('.downvote-button')
+// $saveBtn = $('.save-button');
+// $bottomSection = $('.bottom-section');
+// $searchInput = $('.search-input');
+// $deleteBtn = $('.delete-button');
+// $upvoteBtn = $('.upvote-button')
+// $downvoteBtn = $('.downvote-button')
+$(document).ready(function() {
+  $('.save-button').prop('disabled', true)
+  disableButton()
+})
 
+$('.save-button').on('click', createTask);
+$('.save-button').on('click', disableButton);
 
-$saveBtn.on('click', createTask);
-$bottomSection.on('click', '.delete-button', deleteCard);
-$bottomSection.on('blur', '.task-title', editTitle);
-$bottomSection.on('keydown blur', '.task-body', editBody);
-$bottomSection.on('click', '.upvote-button', increaseQuality);
-$bottomSection.on('click', '.downvote-button', decreaseQuality);
-$searchInput.on('keyup', filterIdeas);
+$('.bottom-section').on('click', '.delete-button', deleteCard);
+$('.bottom-section').on('blur', '.task-title', editTitle);
+$('.bottom-section').on('keydown blur', '.task-body', editBody);
+$('.bottom-section').on('click', '.upvote-button', increaseQuality);
+$('.bottom-section').on('click', '.downvote-button', decreaseQuality);
+$('.search-input').on('keyup', filterIdeas);
 
-
+function disableButton() {
+        $('.title-input, .body-input').keyup(function () {
+            if ($('.title-input').val() == '' || $('.body-input').val() == '') {
+                $('.save-button').prop('disabled', true);
+            } else {
+                $('.save-button').prop('disabled', false);
+            }
+        });
+    }
 
 function createTask() {
   $titleInput = $('.title-input');
@@ -52,7 +65,7 @@ function Card(title, body) {
 }
 
 function prependIdea(card) {
-$bottomSection.prepend (`
+$('.bottom-section').prepend (`
 <article class="idea-card" id=${card.id}>
 <img src ='icons/delete.svg' alt='delete-button' class='delete-button' width='20px' height='20px'>
  <h2 class='task-title' contenteditable>${card.title}</h2>
@@ -70,29 +83,13 @@ function storeIdea(card) {
  localStorage.setItem(card.id, stringifyCard);
 }
 
-// function editTitle() {
-//   debugger;
-//  var currentCard = $(this).closest('.idea-card');
-//  var cardId = currentCard.attr('id');
-//  var parsedCard = JSON.parse(localStorage.getItem(cardId));
-//  parsedCard.title = $(this).text();
-//  storeIdea(parsedCard);
-// }
-
 function editTitle(card) {
-  var key = $(this).parent()[0].id;
-  var currentTask = $(this).closest('.task-title');
-  // getCard(key(date.now));
-  var stringifiedTask = JSON.stringify(currentTask);
-  localStorage.setItem(card.id, stringifiedTask);
-  // update card with userInput
-  // put back into localStorage with storeIdea
+  var currentCard = $(this).closest('.idea-card');
+  var cardId = currentCard.attr('id');
+  var parsedCard = JSON.parse(localStorage.getItem(cardId));
+  parsedCard.title = $(this).text();
+  storeIdea(parsedCard);
 }
-
-// get item from localStorate with ID
-// function getCard(id) {
-  // pull something out of localStorage based on id/key
-  // store nonUpdated card in variable
 
 
 
@@ -125,7 +122,6 @@ storeQuality(cardId, upQualityID.text())
 }
 
 function decreaseQuality() {
-  console.log(downQualityID)
 var downQualityID = $(this).parent().find('#quality');
 var cardId = downQualityID.parent().attr('id');
 if (downQualityID.text() === ' genius') {
@@ -136,13 +132,17 @@ if (downQualityID.text() === ' genius') {
  storeQuality(cardId, downQualityID.text());
 }
 
+// declare a new variable importanceLevels ['critical', 'high', 'normal', 'low', 'none'];
+
+
+
 function deleteCard() {
-  console.log(this)
  var deleteCard = $(this).closest('.idea-card');
  var cardId = deleteCard.attr('id');
  deleteCard.remove();
  localStorage.removeItem(cardId);
 }
+
 
 function filterIdeas() {
     $searchInput = $('.search-input').val().toUpperCase();
